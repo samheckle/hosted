faa72dd5c4244f418f02bf1a2811681d730432cd
samheckle <samheckle@yahoo.com>
2022-05-09 02:28:44 -0400

    fixed cors or had a browser extension that worked also

--- gen-blackout-poetry/main.js
+++ gen-blackout-poetry/main.js
@@ -1,34 +1,10 @@
-// const myHeaders = new Headers();
-
-// const myRequest = new Request("https://minimal-python3-flask.glitch.me/", {
-//   method: 'GET',
-//   headers: myHeaders,
-//   mode: 'no-cors',
-//   cache: 'default',
-// });
-
-// fetch(myRequest)
-//     .then((response) => response.json())
-//     .then((data) => console.log(data))
-
-fetch("http://minimal-python3-flask.glitch.me/", {
+fetch("https://planet-green-flute.glitch.me/", {
+  method: 'GET',
   mode: "cors",
   headers: {
     "Access-Control-Allow-Origin": "*",
   },
 })
-  .then((response) => response.json())
-  .then((data) => console.log(data));
-// fetch(url, {
-//   method: "POST", // *GET, POST, PUT, DELETE, etc.
-//   mode: "cors", // no-cors, *cors, same-origin
-//   cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-//   credentials: "same-origin", // include, *same-origin, omit
-//   headers: {
-//     "Content-Type": "application/json",
-//     // 'Content-Type': 'application/x-www-form-urlencoded',
-//   },
-//   redirect: "follow", // manual, *follow, error
-//   referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-//   body: JSON.stringify(data), // body data type must match "Content-Type" header
-// });
+  .then((response) => {return response.json()})
+  .then((data) => console.log(data))
+  .catch((e) => console.log(e))
