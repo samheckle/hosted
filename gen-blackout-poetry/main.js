e9aa37f03d5f88829213f68908737ae5a68ccb32
samheckle <samheckle@yahoo.com>
2022-05-09 02:11:02 -0400

    updated no cors

--- gen-blackout-poetry/main.js
+++ gen-blackout-poetry/main.js
@@ -11,7 +11,7 @@
 //     .then((response) => response.json())
 //     .then((data) => console.log(data))
 
-fetch("https://minimal-python3-flask.glitch.me/", {
+fetch("http://minimal-python3-flask.glitch.me/", {
   mode: "cors",
   headers: {
     "Access-Control-Allow-Origin": "*",
